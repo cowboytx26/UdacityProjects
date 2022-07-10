0ceb7d8a831224a82269aeabd204e4ac44a39fa8
Brian W Wiatrek <brianwiatrek@aol.com>
2022-07-09 20:48:50 -0500

    modified the parallelwebcrawler

---
+++ starter/webcrawler/src/main/java/com/udacity/webcrawler/CrawlTask.java
@@ -0,0 +1,92 @@
+package com.udacity.webcrawler;
+
+import com.udacity.webcrawler.json.CrawlResult;
+import com.udacity.webcrawler.parser.PageParser;
+import com.udacity.webcrawler.parser.PageParserFactory;
+
+import java.time.Clock;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.*;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ConcurrentSkipListSet;
+
+//Check on what the implementation of a task class should look like in the lesson on forkjoin
+//I need to have a method called compute for the thread task and possibly another method that does some type of work
+//I think this second method is optional - actually the second  method is the constructor and I have to have it
+//The compute method should return a CrawlResult
+//I will need to call a new CrawlTask within the compute method of this class to do the recursion
+//The initial call to CrawlTask and compute should be from the ParallelWebCrawler
+public class CrawlTask extends RecursiveAction {
+
+    private final String url;
+    private final Clock clock;
+    private final PageParserFactory parserFactory;
+    private final Duration timeout;
+    private final int popularWordCount;
+    private final int maxDepth;
+    private final List<Pattern> ignoredUrls;
+    private final Map<String, Integer> counts;
+    private final Set<String> visitedUrls;
+    private final ForkJoinPool pool;
+
+    //I should be passing in the counts collection and visited URL's just like in the sequential version of this call
+    //I guess I can pass in clock.  The deadline is a function of the clock plus the timeout, so I will need to test it
+    //out at every fork.
+    //url, clock, parserFactoryParallel, timeout, popularWordCount, maxDepth, ignoredUrls, counts, visitedUrls
+    public CrawlTask(String url, Clock clock, PageParserFactory parserFactory, Duration timeout, int popularWordCount,
+                     int maxDepth, List<Pattern> ignoredUrls, Map<String, Integer> counts,
+                     Set<String> visitedUrls, ForkJoinPool pool) {
+        this.url = url;
+        this.clock = clock;
+        this.parserFactory = parserFactory;
+        this.timeout = timeout;
+        this.popularWordCount = popularWordCount;
+        this.maxDepth = maxDepth;
+        this.ignoredUrls = ignoredUrls;
+        this.counts = counts;
+        this.visitedUrls = visitedUrls;
+        this.pool = pool;
+    }
+
+    //Compute doesn't need to return anything so long as it updates the counts and visitedUrls collections
+    @Override
+    protected void compute() {
+
+        Instant deadline = clock.instant().plus(timeout);
+        if (maxDepth == 0 || clock.instant().isAfter(deadline)) {
+            return;
+        }
+
+        for (Pattern pattern : ignoredUrls) {
+            if (pattern.matcher(url).matches()) {
+                return;
+            }
+        }
+
+        if (visitedUrls.contains(url)) {
+            return;
+        }
+
+        visitedUrls.add(url);
+        PageParser.Result result = parserFactory.get(url).parse();
+        List<String> sublinks =result.getLinks();
+
+        for (ConcurrentMap.Entry<String, Integer> e : result.getWordCounts().entrySet()) {
+            counts.compute(e.getKey(), (k,v)->(v==null)?e.getValue():e.getValue()+v);
+        }
+
+        for (String link : sublinks) {
+            CrawlTask task = new CrawlTask(url, clock, parserFactory, timeout, popularWordCount,
+                    maxDepth - 1, ignoredUrls, counts, visitedUrls, pool);
+            pool.invoke(task);
+        }
+        return;
+    }
+}
--- starter/webcrawler/src/main/java/com/udacity/webcrawler/ParallelWebCrawler.java
+++ starter/webcrawler/src/main/java/com/udacity/webcrawler/ParallelWebCrawler.java
@@ -11,9 +11,12 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.concurrent.ConcurrentSkipListSet;
-import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.*;
+import java.util.*;
 import java.util.stream.Collectors;
+import com.udacity.webcrawler.CrawlTask;
+import com.udacity.webcrawler.parser.PageParserFactory;
+import java.util.regex.Pattern;
 
 /**
  * A concrete implementation of {@link WebCrawler} that runs multiple threads on a
@@ -21,25 +24,54 @@ import java.util.stream.Collectors;
  */
 final class ParallelWebCrawler implements WebCrawler {
   private final Clock clock;
+  private final PageParserFactory parserFactoryParallel;
   private final Duration timeout;
   private final int popularWordCount;
   private final ForkJoinPool pool;
+  private final int maxDepth;
+  private final List<Pattern> ignoredUrls;
 
   @Inject
   ParallelWebCrawler(
-      Clock clock,
-      @Timeout Duration timeout,
-      @PopularWordCount int popularWordCount,
-      @TargetParallelism int threadCount) {
+          Clock clock,
+          PageParserFactory parserFactoryParallel,
+          @Timeout Duration timeout,
+          @PopularWordCount int popularWordCount,
+          @TargetParallelism int threadCount,
+          @MaxDepth int maxDepth,
+          @IgnoredUrls List<Pattern> ignoredUrls) {
     this.clock = clock;
+    this.parserFactoryParallel = parserFactoryParallel;
     this.timeout = timeout;
     this.popularWordCount = popularWordCount;
     this.pool = new ForkJoinPool(Math.min(threadCount, getMaxParallelism()));
+    this.maxDepth = maxDepth;
+    this.ignoredUrls = ignoredUrls;
   }
 
   @Override
   public CrawlResult crawl(List<String> startingUrls) {
-    return new CrawlResult.Builder().build();
+    Instant deadline = clock.instant().plus(timeout);
+    //The datastructures below need to be thread safe
+    Map<String, Integer> counts = Collections.synchronizedMap(new HashMap<>());
+    Set<String> visitedUrls = Collections.synchronizedSet(new HashSet<>());
+
+    for (String url : startingUrls) {
+      CrawlTask task = new CrawlTask(url, clock, parserFactoryParallel, timeout, popularWordCount, maxDepth, ignoredUrls, counts, visitedUrls, pool);
+      pool.invoke(task);
+    }
+
+    if (counts.isEmpty()) {
+      return new CrawlResult.Builder()
+              .setWordCounts(counts)
+              .setUrlsVisited(visitedUrls.size())
+              .build();
+    }
+
+    return new CrawlResult.Builder()
+            .setWordCounts(WordCounts.sort(counts, popularWordCount))
+            .setUrlsVisited(visitedUrls.size())
+            .build();
   }
 
   @Override
